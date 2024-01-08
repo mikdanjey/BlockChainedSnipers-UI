1221d21ce5b99c24f4b08e300d35be6cf8da0631
Mikdan Jey <mikdanjey@gmail.com>
2024-01-09 01:00:50 +0530

    ok#

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export',
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "mikdantech.local",
-            },
-        ],
-    },
-    // experimental: {
-    //   forceSwcTransforms: true,
-    // },
+  // output: 'export',
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mikdantech.local",
+      },
+    ],
+  },
+  // experimental: {
+  //   forceSwcTransforms: true,
+  // },
 };
 
 module.exports = nextConfig;
